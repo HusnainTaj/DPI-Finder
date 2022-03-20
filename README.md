3ed860e343128576a2c17c6e4500ee2dce5deb7a
Hacker <85726252+HusnainTaj@users.noreply.github.com>
2022-03-20 19:23:21 +0500

    made it look much better

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# DPI-Finder
+# DPI Finder
 Simple web app to find DPI of a mouse.
+
+# How to Use
+1. Enter a distance in inches (larger value prefered).
+2. Click and hold with any of your mouse button
+3. Move your mouse the sepcified distance (use a ruler/measuring tape).
+4. Let go of the mouse button.
+
+## Note
+Generally, DPI of a mouse is a multiple of 50 e.g. 800, 950, 1000 etc, so measure your DPI 3-4 times and round the average value.
--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 "use strict";
 
-document.addEventListener('pointerlockchange', ()=>
-{
-  if (document.pointerLockElement === document.body) 
-  {
-    // console.log('The pointer lock status is now locked');
-    document.addEventListener("mousemove", PLMouseMove, false);
-  }
-  else 
-  {
-    // console.log('The pointer lock status is now unlocked');  
-    document.removeEventListener("mousemove", PLMouseMove, false);
-  }
+document.addEventListener('pointerlockchange', () => {
+	if (document.pointerLockElement === document.body) {
+		console.log('The pointer lock status is now locked');
+		document.addEventListener("mousemove", PLMouseMove, false);
+	}
+	else {
+		console.log('The pointer lock status is now unlocked');
+		document.removeEventListener("mousemove", PLMouseMove, false);
+	}
 }, false);
 
-document.addEventListener("mouseup", ()=>
-{
-    if(document.pointerLockElement)
-    {
-        // $("#pixelMovedCount").text(Math.abs(x))
-        // let dpi = Math.abs(x) / $("#distance").val()
-        // $("#resultantDpi").text(dpi)
-
-        document.exitPointerLock();
-        x=0;
-        y=0;
-    }
+document.addEventListener("mouseup", () => {
+	if (document.pointerLockElement) {
+		document.exitPointerLock();
+		x = 0;
+		y = 0;
+	}
 });
 
 let x = 0;
 let y = 0;
 
-function PLMouseMove(e)
-{
-    x+=e.movementX;
-    y+=e.movementY;
-    // console.log(e.movementX);
-    $("#pixelMovedCount").text(Math.abs(x))
-    let dpi = Math.abs(x) / $("#distance").val()
-    $("#resultantDpi").text(dpi)
+function PLMouseMove(e) {
+	x += e.movementX;
+	y += e.movementY;
+	// console.log(e.movementX);
+	$("#pixelMovedCount").val(Math.abs(x))
+	let dpi = Math.abs(x) / parseInt($("#distance").val())
+	$("#resultantDpi").text(dpi.toFixed(2))
 }
 
-$(()=>{
+$(() => {
 
-    let box = $("#box");
-    let pixelMovedCount = $("#pixelMovedCount");
+	$("body").on("mousedown", (e) => {
+		// dont lock the pointer if user is inc/dec distance
+		if (!$(".material-icons:hover").length)
+			document.body.requestPointerLock();
+	});
 
-    box.on("mousedown", (e)=>{
-        document.body.requestPointerLock();
-    });
+	$("body").on("click", ".minus", (e) => {
+		let input = $(e.currentTarget).parent().children("input")
+		input.val(parseInt(input.val()) - 1)
+	});
+
+	$("body").on("click", ".add", (e) => {
+		let input = $(e.currentTarget).parent().children("input")
+		input.val(parseInt(input.val()) + 1)
+	});
 })
